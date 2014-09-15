990c162b1f6b5c662d1c6238e28863992ef5446d
Chad Bartels <chad.bartels@gmail.com>
2014-09-15 19:34:43 -0400

    Remove settings

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,12 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      # :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_access_key_id      => 'AKIAIQDYDJUZSSI7WCBQ',
-      # :aws_secret_access_key  => ENV['S3_SECRET']
-      :aws_secret_access_key  => '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'
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET']
       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
-  # config.fog_directory  = ENV['S3_BUCKET']
-  config.fog_directory  = 'pocket-stable'
+  config.fog_directory  = ENV['S3_BUCKET']
 end
