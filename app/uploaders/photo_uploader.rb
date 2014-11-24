366a499fe65fecdab10e70438bcbe9f99fff7509
Chad Bartels <chad.bartels@gmail.com>
2014-11-24 11:01:46 -0500

    scaling photo uploads

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,7 +4,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -25,7 +25,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [364,234]
+  process :resize_to_fit => [768,400]
   #
   # def scale(width, height)
   #   # do something
